8e6477b382318285dbfc2e6629bdf507e2534dd8
Rene Krizak <rene.krizak@gmail.com>
2024-12-01 11:41:44 +0100

    day 1

--- solutions/day01.py
+++ solutions/day01.py
@@ -1,8 +1,38 @@
 from utils.SolutionRunner import SolutionRunner
+from collections import Counter
 
 class Day01(SolutionRunner):
     def part1(self):
-        return "test"
-    
+        data = self.input_reader.read_lines()
+        left_list = []
+        right_list = []
+
+        for line in data:
+            left, right = map(int, line.split())
+            left_list.append(left)
+            right_list.append(right)
+
+        left_list = sorted(left_list)
+        right_list = sorted(right_list)
+
+        dist = 0
+        for i in range(len(left_list)):
+            dist += abs(left_list[i] - right_list[i])
+        return dist
+
     def part2(self):
-        return "test"
+        data = self.input_reader.read_lines()
+        left_list = []
+        right_list = []
+
+        for line in data:
+            left, right = map(int, line.split())
+            left_list.append(left)
+            right_list.append(right)
+
+        right_counts = Counter(right_list)
+
+        similarity_score = 0
+        for num in left_list:
+            similarity_score += num * right_counts.get(num, 0)
+        return similarity_score
---
+++ solutions/day02.py
@@ -0,0 +1,8 @@
+from utils.SolutionRunner import SolutionRunner
+
+class Day02(SolutionRunner):
+    def part1(self):
+        return ""
+    
+    def part2(self):
+        return ""
