3d5effea02371ef446fbb833701b30509a2294f8
Rene Krizak <rene.krizak@gmail.com>
2024-12-02 10:18:00 +0100

    day 2 solution

--- solutions/day02.py
+++ solutions/day02.py
@@ -1,8 +1,52 @@
 from utils.SolutionRunner import SolutionRunner
 
 class Day02(SolutionRunner):
+
+    def is_safe(self, entry):
+        inc_flag = True
+        dec_flag = True
+
+        for j in range(len(entry) - 1):
+            diff = entry[j+1] - entry[j]
+            if 1 <= diff <= 3:
+                dec_flag = False
+            elif -3 <= diff <= -1:
+                inc_flag = False
+            else:
+                inc_flag = False
+                dec_flag = False
+                break
+        return inc_flag or dec_flag
+
     def part1(self):
-        return ""
+        data = self.input_reader.read_lines()
+        data = [[int(num) for num in nums.split()] for nums in data]
+        
+        count = 0
+        for entry in data:
+            if self.is_safe(entry):
+                count += 1
+            
+        return count
     
+
     def part2(self):
-        return ""
+        data = self.input_reader.read_lines()
+        data = [[int(num) for num in nums.split()] for nums in data]
+
+        unsafe = []
+        count = 0
+        for entry in data:
+            if self.is_safe(entry):
+                count += 1
+            else:
+                unsafe.append(entry)
+        
+        for entry in unsafe:
+            for i in range(len(entry)):
+                modified = entry[:i] + entry[i + 1:]
+                if self.is_safe(modified):
+                    count += 1
+                    break
+
+        return count
